658ff011abfb26790d8007a557f075f3a634aff1
Amelie47 <ameliemouillacpro@gmail.com>
2021-02-19 10:20:23 +0100

    Initial commit

---
+++ app/src/main/java/com/ameliemouillac/gmail/tp02_amelie_mouillac/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ameliemouillac.gmail.tp02_amelie_mouillac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
